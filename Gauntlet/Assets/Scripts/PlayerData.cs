4bdfda3a7bb47bcd8a15142ada2f18074a37c7fa
Christian Rucki <ruckichristian@hotmail.com>
2023-04-05 21:18:21 -0700

    Scriptable Objects/Prefabs Created

---
+++ Gauntlet/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    //Shows up in the UI
+    public string className;
+    public float health;
+    public int score;
+    public int numberOfKeys;
+    public int numberOfPotions;
+
+    //Game Objects
+    public GameObject characterPrefab;
+    public GameObject shotPrefab;
+
+    //Does not show up in UI
+    public float runSpeed;
+    public float armorStrength;
+    public float shotStrength;
+    public float shotTravelSpeed;
+    public float meleeStrength;
+    public float magicVsMonsters;
+    public float magicVsGenerators;
+    public float potionShotVsMonsters;
+    public float potionShotVsGenerators;
+}
