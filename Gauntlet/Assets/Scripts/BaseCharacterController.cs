8f8b0d5d369cbb45d619f4711d84053dcbab2479
Marant650 <47579237+Marant650@users.noreply.github.com>
2023-04-17 22:47:37 -0700

    rigidbody movement

--- Gauntlet/Assets/Scripts/BaseCharacterController.cs
+++ Gauntlet/Assets/Scripts/BaseCharacterController.cs
@@ -5,26 +5,46 @@ using UnityEngine.InputSystem;
 
 public class BaseCharacterController : MonoBehaviour
 {
-    Vector2 moveInput;    
+    Vector2 moveInput;
+    Vector3 moveDirection;
     public PlayerData warrior;
     private float runSpeed;
+    private Rigidbody rb;
+    
     
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         runSpeed = warrior.runSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.velocity = new Vector3(moveInput.x, 0, moveInput.y) * runSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(moveInput.x , 0, moveInput.y) * runSpeed * Time.deltaTime);
+        if(moveInput != Vector2.zero)
+        {
+            transform.forward = new Vector3(moveInput.x,0,moveInput.y);
+        }
+       // transform.Translate(new Vector3(moveInput.x , 0, moveInput.y) * runSpeed * Time.deltaTime);
+
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();        
         Debug.Log(moveInput);
+    }
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+
     }
 }
