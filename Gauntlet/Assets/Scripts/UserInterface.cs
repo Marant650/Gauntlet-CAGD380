796f38f542d9c23dbc53206482b0f9b2ebb4bbe1
Christian Rucki <ruckichristian@hotmail.com>
2023-04-18 13:44:02 -0700

    Started User Interface

---
+++ Gauntlet/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+    public PlayerData warrior;
+    public PlayerData valkyrie;
+    public PlayerData wizard;
+    public PlayerData elf;
+}
