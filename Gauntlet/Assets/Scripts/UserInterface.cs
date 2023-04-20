a81b2c0803440543505d7607878e961ad7abc593
Christian Rucki <ruckichristian@hotmail.com>
2023-04-20 16:30:50 -0700

    Character Select Screen

--- Gauntlet/Assets/Scripts/UserInterface.cs
+++ Gauntlet/Assets/Scripts/UserInterface.cs
@@ -6,7 +6,53 @@ using UnityEngine.UI;
 public class UserInterface : MonoBehaviour
 {
     public PlayerData warrior;
-    public PlayerData valkyrie;
     public PlayerData wizard;
     public PlayerData elf;
+    public PlayerData valkyrie;
+
+    public Animator warriorAnimation;
+    public Animator wizardAnimation;
+    public Animator elfAnimation;
+    public Animator valkyrieAnimation;
+
+
+    private void Start()
+    {
+        warriorAnimation.enabled = false;
+        wizardAnimation.enabled = false;
+        elfAnimation.enabled = false;
+        valkyrieAnimation.enabled = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            warriorAnimation.enabled = true;
+            wizardAnimation.enabled = false;
+            elfAnimation.enabled = false;
+            valkyrieAnimation.enabled = false;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            warriorAnimation.enabled = false;
+            wizardAnimation.enabled = true;
+            elfAnimation.enabled = false;
+            valkyrieAnimation.enabled = false;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            warriorAnimation.enabled = false;
+            wizardAnimation.enabled = false;
+            elfAnimation.enabled = true;
+            valkyrieAnimation.enabled = false;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            warriorAnimation.enabled = false;
+            wizardAnimation.enabled = false;
+            elfAnimation.enabled = false;
+            valkyrieAnimation.enabled = true;
+        }
+    }
 }
