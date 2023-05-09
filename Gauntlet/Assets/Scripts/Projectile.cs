768b924cdd481b76edce4de747aded043373fbde
Marant650 <47579237+Marant650@users.noreply.github.com>
2023-05-09 13:31:44 -0700

    enemies

--- Gauntlet/Assets/Scripts/Projectile.cs
+++ Gauntlet/Assets/Scripts/Projectile.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     private float _projectileSpeed;
+    private int score;
+    private BaseCharacterController player;
 
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseCharacterController>();
         _projectileSpeed = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseCharacterController>().character.shotTravelSpeed;
     }
 
@@ -18,7 +21,11 @@ public class Projectile : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            player.character.score += 10;
+            Destroy(gameObject);
+        }
         
-        Destroy(gameObject);
     }
 }
