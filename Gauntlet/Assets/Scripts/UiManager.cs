7783c633069e6a40cf5120fa81a44c6de8bc5011
Christian Rucki <ruckichristian@hotmail.com>
2023-05-09 13:31:50 -0700

    User Interface

--- Gauntlet/Assets/Scripts/UiManager.cs
+++ Gauntlet/Assets/Scripts/UiManager.cs
@@ -9,12 +9,37 @@ public class UiManager : MonoBehaviour
     public CharacterSelect characterSelect;
 
     public Text playerOneHealth;
+    public Text playerOneScore;
 
     private void Start()
+    {
+        SetPlayerOneUI();
+    }
+
+    private void SetPlayerOneUI()
     {
         if(characterSelect.playerOneClass == "warrior")
         {
             playerOneHealth.color = Color.red;
+            playerOneScore.color = Color.red;
+        }
+        
+        if (characterSelect.playerOneClass == "wizard")
+        {
+            playerOneHealth.color = Color.yellow;
+            playerOneScore.color = Color.yellow;
+        }
+        
+        if (characterSelect.playerOneClass == "elf")
+        {
+            playerOneHealth.color = Color.green;
+            playerOneScore.color = Color.green;
+        }
+
+        if (characterSelect.playerOneClass == "valkyrie")
+        {
+            playerOneHealth.color = Color.blue;
+            playerOneScore.color = Color.blue;
         }
     }
 }
