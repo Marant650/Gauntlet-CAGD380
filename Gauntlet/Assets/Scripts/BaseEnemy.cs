020214b1559dfb9bd9264b4ec18436c0cb4d929d
Christian Rucki <ruckichristian@hotmail.com>
2023-05-16 00:09:43 -0700

    Attempt to fix AI targeting

--- Gauntlet/Assets/Scripts/BaseEnemy.cs
+++ Gauntlet/Assets/Scripts/BaseEnemy.cs
@@ -7,19 +7,39 @@ public class BaseEnemy : MonoBehaviour
 {
     NavMeshAgent agent;
 
-    public GameObject player1;
+    public GameObject[] _players;
+
+    public GameObject chaseTarget;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        player1 = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = player1.transform.position;
+        _players = GameObject.FindGameObjectsWithTag("Player");
+        CalculateClosestPlayer();
+        
+        agent.destination = chaseTarget.transform.position;
     }
-   
+
+    private void CalculateClosestPlayer()
+    {
+        float playerOneDistance = Vector3.Distance(transform.position, _players[0].transform.position);
+        float playerTwoDistance = Vector3.Distance(transform.position, _players[1].transform.position);
+        float playerThreeDistance = Vector3.Distance(transform.position, _players[2].transform.position);
+        float playerFourDistance = Vector3.Distance(transform.position, _players[3].transform.position);
+
+        if(playerOneDistance > playerTwoDistance)
+        {
+            chaseTarget = _players[1];
+        }
+        else if(playerOneDistance < playerTwoDistance)
+        {
+            chaseTarget = _players[0];
+        }
+    }
 }
--- Gauntlet/Assets/Scripts/Ghost.cs
+++ Gauntlet/Assets/Scripts/Ghost.cs
@@ -4,21 +4,9 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    private PlayerData character;
     public GameObject player;
     public int damage = 10;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+  
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
